bc7009050c11aa788ea266405098cb06725e0345
francescopiscaglia <piscagliafrancesco@gmail.com>
2024-11-04 17:08:20 +0100

    add tags filter

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -18,7 +18,6 @@ const postsList = (req, res) => {
 
 // index
 const index = (req, res) => {
-
     res.sendFile(path.resolve(__dirname, "../public/html/list.html"));
 };
 
@@ -29,12 +28,21 @@ const show = (req, res) => {
     // ricerco all'interno dei posts in base allo slug inserito
     const post = posts.find(post => post.slug == req.params.slug);
 
+    // controllo per eventuale slug sbagliato
     if (!post) {
         return res.status(404).json({
             error: "404! Not found"
         });
     };
-    
+
+    // filtro per tag
+    if (req.query.tags) {
+        const tagsFilter = posts.filter(post => post.tags.includes(req.query.tags))
+        return res.json({
+            data: tagsFilter
+        });
+    };  
+
     // gestico la risposta in formato json
     res.json({
         data: post
